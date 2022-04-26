e627b1a9afdf5323caf0889e67223d289bd53db0
Mo Sabouni <M_95_@hotmail.com>
2022-04-26 21:07:40 +0200

    replaced fetch method to use try catch with cleaner code in app.js

--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -12,24 +12,16 @@ app.get('/', (req, res) => {
 });
 
 app.post('/weather', async (req, res) => {
-  // getting city name from the request
-  const cityName = req.body.cityName;
-  // fetching weather information
-  const weatherInfo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}`)
-  .then(res => res.json())
-  .catch(err => {
-    console.error(err)
-    });
-    // checking if response has the right property if not it return city not found
-    if (!weatherInfo.name) {
-       return res.status(404).json({
-          weatherText: "City is not found!"
-        });
-    }
-    // creating response object and sending it back with correct status
+  try {
+    const cityName = req.body.cityName;
+    const weatherFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}`)
+    const weatherInfo = await weatherFetch.json();
     const weatherRes = {cityName: `${weatherInfo.name}`, temp:`${weatherInfo.main.temp}`};
     res.status(200).json(weatherRes);
-  
+  } catch (err) {
+    res.status(404).json({ weatherText: "City is not found!" });
+  }
+
 });
 
 export default app
