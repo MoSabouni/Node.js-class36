55d4b95920c83b374ee85a98c6c9ba5e2503500c
Mo Sabouni <M_95_@hotmail.com>
2022-05-25 12:00:24 +0200

    Fixes based on mentors feedback

--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -1,27 +1,31 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import {keys} from "./sources/keys.js";
-import fetch from "node-fetch"
+import { keys } from "./sources/keys.js";
+import fetch from "node-fetch";
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/', (req, res) => {
-  res.send('Hello from backend to front end');
+app.get("/", (req, res) => {
+  res.send("Hello from backend to front end");
 });
 
-app.post('/weather', async (req, res) => {
+app.post("/weather", async (req, res) => {
   try {
     const cityName = req.body.cityName;
-    const weatherFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}`)
+    if (!cityName) {
+      return res.status(400).json({ msg: 'no city name was provided.' });
+    const weatherFetch = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}&units=metric`
+    );
     const weatherInfo = await weatherFetch.json();
-    const weatherRes = {cityName: `${weatherInfo.name}`, temp:`${weatherInfo.main.temp}`};
+    const weatherRes = {
+      weatherText: `${weatherInfo.name} current temprature is${weatherInfo.main.temp} degrees`,
+    };
     res.status(200).json(weatherRes);
   } catch (err) {
     res.status(404).json({ weatherText: "City is not found!" });
   }
-
 });
 
-export default app
+export default app;
